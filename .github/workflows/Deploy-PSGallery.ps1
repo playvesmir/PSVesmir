3344cef5528634d5cb45a531ac7e189fcf42059d
Sergey <sergey@xmethod.tech>
2020-05-11 23:26:03 -0700

    asd2

--- .github/workflows/Deploy-PSGallery.ps1
+++ .github/workflows/Deploy-PSGallery.ps1
@@ -1,7 +1,7 @@
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
 try{
-    Publish-Module -Path "$GITHUB_WORKSPACE\PSVesmir" -NuGetApiKey "$env:POWERSHELL_GALLERY_API_KEY" -ErrorAction Stop -Force -Debug
+    Publish-Module -Path "$env:GITHUB_WORKSPACE\PSVesmir" -NuGetApiKey "$env:POWERSHELL_GALLERY_API_KEY" -ErrorAction Stop -Force -Debug
     Write-Host "Cool, we done here."
 }
 catch { 
